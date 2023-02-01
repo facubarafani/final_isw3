7f5db487104f6f87d5437b7246bd1e0ddcf8e41c
Facundo Barafani <facundobarafani@facundos-mbp.home>
2023-02-01 16:17:45 -0300

    Change test database url connection to env fil

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,25 +1,25 @@
 module.exports = app => {
-    const todo = require("../controllers/todo.controller.js");
-  
-    var router = require("express").Router();
-  
-    // Create a new Todo
-    router.post("/", todo.create);
+  const todo = require("../controllers/todo.controller.js");
 
-    // Get all Todos
-    router.get("/", todo.get)
-  
-    // Retrieve a single Todo with id
-    router.get("/:id", todo.findOne);
-  
-    // Update a Todo
-    router.put("/:id", todo.update);
-  
-    // Delete a Tutorial with id
-    router.delete("/:id", todo.delete);
-  
-    // Delete all Tutorials
-    router.delete("/", todo.deleteAll);
-  
-    app.use('/api/todo', router);
-  };
+  var router = require("express").Router();
+
+  // Create a new Todo
+  router.post("/", todo.create);
+
+  // Get all Todos
+  router.get("/", todo.get)
+
+  // Retrieve a single Todo with id
+  router.get("/:id", todo.findOne);
+
+  // Update a Todo
+  router.put("/:id", todo.update);
+
+  // Delete a Tutorial with id
+  router.delete("/:id", todo.delete);
+
+  // Delete all Tutorials
+  router.delete("/", todo.deleteAll);
+
+  app.use('/api/todo', router);
+};
--- tests/todo.controller.test.js
+++ tests/todo.controller.test.js
@@ -6,44 +6,46 @@ const db = require("../models");
 const ToDo = db.todo;
 
 beforeEach((done) => {
-	mongoose.connect(
-		"mongodb://root:example@localhost:27017/",
-		{ useNewUrlParser: true,
-            dbName: "isw3_db" },
-		() => done()
-	)
+  mongoose.connect(
+    process.env.MONGO_URL,
+    {
+      useNewUrlParser: true,
+      dbName: "isw3_db"
+    },
+    () => done()
+  )
 })
 
 afterEach((done) => {
-	mongoose.connection.db.dropDatabase(() => {
-		mongoose.connection.close(() => done())
-	})
+  mongoose.connection.db.dropDatabase(() => {
+    mongoose.connection.close(() => done())
+  })
 })
 
 describe("API ENDPOINT TEST", () => {
-    test("/GET ALL TODO", (done) => {
-      request(app)
-        .get("/api/todo")
-        .expect("Content-Type", /json/) // Ensure service returns JSON format.
-        .expect(200) // Ensure service call returns status code 200 (OK).
-        .end((err, res) => {
-          if (err) return done(err);
-          return done();
-        });
+  test("/GET ALL TODO", (done) => {
+    request(app)
+      .get("/api/todo")
+      .expect("Content-Type", /json/) // Ensure service returns JSON format.
+      .expect(200) // Ensure service call returns status code 200 (OK).
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
 
-    });
+  });
 
-    test("/POST TODO", async () => {
-        const payload = {body: 'This is a test', title: 'This is a test'};
+  test("/POST TODO", async () => {
+    const payload = { body: 'This is a test', title: 'This is a test' };
 
-        const res = await request(app)
-            .post('/api/todo')
-            .send(payload)
-            .set('Content-Type', 'application/json')
-            .set('Accept', 'application/json')
+    const res = await request(app)
+      .post('/api/todo')
+      .send(payload)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
 
-        expect(200) // Ensure service call returns status code 200 (OK).
-        expect(res.body.title).toBe('This is a test');
-        expect(res.body._id.length).toEqual(24); // Validate new ToDo object id has 24 characters (https://www.mongodb.com/docs/manual/reference/method/ObjectId/#:~:text=A%2024%20character%20hexadecimal%20string%20value%20for%20the%20new%20ObjectId.&text=Optional.,-The%20integer%20value)
-    })
+    expect(200) // Ensure service call returns status code 200 (OK).
+    expect(res.body.title).toBe('This is a test');
+    expect(res.body._id.length).toEqual(24); // Validate new ToDo object id has 24 characters (https://www.mongodb.com/docs/manual/reference/method/ObjectId/#:~:text=A%2024%20character%20hexadecimal%20string%20value%20for%20the%20new%20ObjectId.&text=Optional.,-The%20integer%20value)
+  })
 });
